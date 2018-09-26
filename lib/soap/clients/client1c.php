6c19a7d75ed094eb4d0bf1d88f6317576f2dce27
Magomedali <web-ali@yandex.ru>
2018-09-26 09:59:14 +0300

    version 1.0.0

--- lib/soap/clients/client1c.php
+++ lib/soap/clients/client1c.php
@@ -9,7 +9,7 @@ namespace Ali\Logistic\soap\clients;
 abstract class Client1C 
 {
 	
-	protected static $wsdl_url = "http://217.76.41.228/basetest/ws/ws1.1cws?wsdl";
+	protected static $wsdl_url = "http://217.76.41.228/exp/ws/ws1.1cws?wsdl";
 	protected static $user = "Федулов Денис";
 	protected static $password = "TporgS4";
 
